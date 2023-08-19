951b6eee457f87700354733fefcb7960909a6e62
S1LV3Rman <conik1999@gmail.com>
2023-08-19 16:44:26 +0300

    done

---
+++ Assets/Source/FuelContainer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Source
+{
+    public class FuelContainer : MonoBehaviour
+    {
+        [SerializeField] private float _fuelAmount;
+
+        public float GetFuelAmount() => _fuelAmount;
+        
+        public float RequestFuel(float requestedAmount)
+        {
+            var fuelToGive = Mathf.Min(requestedAmount, _fuelAmount);
+            _fuelAmount -= fuelToGive;
+            return fuelToGive;
+        }
+
+        public void AddFuel(float amountToAdd)
+        {
+            _fuelAmount += amountToAdd;
+        }
+    }
+}
---
+++ Assets/Source/LocomotiveEngine.cs
@@ -0,0 +1,24 @@
+using Source;
+using UnityEngine;
+
+public class LocomotiveEngine : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private FuelContainer _fuelContainer;
+    
+    [SerializeField] private float _fuelPerSecond;
+    [SerializeField] private float _force;
+    
+    void Update()
+    {
+        var direction = Input.GetAxis("Vertical");
+        if (!Mathf.Approximately(direction, 0f))
+        {
+            var requiredFuel = Time.deltaTime * _fuelPerSecond;
+            var fuel = _fuelContainer.RequestFuel(requiredFuel);
+            var forceMultiplier = _force * fuel / requiredFuel;
+            var force = Vector2.up * forceMultiplier * direction;
+            _rigidbody.AddForce(force);
+        }
+    }
+}
